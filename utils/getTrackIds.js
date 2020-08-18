9eff06b54793d11d0b9aa748b1942d9eeb7fecf7
Devin Rucks <blindnives@gmail.com>
2020-08-18 18:32:50 -0400

    Added an array prototype to take care of converting the 1D array of track Ids into a 2D array

--- utils/getTrackIds.js
+++ utils/getTrackIds.js
@@ -6,23 +6,53 @@
  * @return {Array} The array of track ids
  */
 const getTrackIds = async (spotifyApi, artistIds, country) => {
+
+     // This contains just an array of track ids
      let allTopTracks = [];
 
      await asyncForEach(artistIds, async (id) => {
           try {
 
                const artistTopTracks = await spotifyApi.getArtistTopTracks(id, country)
+
                artistTopTracks.body.tracks.forEach((track) => {
                     allTopTracks.push(track.id)
                })
+
           } catch (err) {
                console.log(`Error from getTrackIds: ${err}`)
           }
      })
 
+     // Since some of the future api calls will have a max of 50, the 1D array will need to be converted into
+     // a 2D array with the width of 50.
+     allTopTracks.convertTo2D(50)
+
+     console.log(allTopTracks)
      return allTopTracks;
 
 };
+
+Array.prototype.convertTo2D = function (cols) {
+     var copy = this.slice(0); // Copy all elements.
+     this.length = 0; // Clear out existing array.
+
+     let rows = (copy.length / 50);
+     if (rows < 1) rows = 1;
+
+     for (let r = 0; r < rows; r++) {
+          let row = [];
+          for (let c = 0; c < cols; c++) {
+               let i = r * cols + c;
+               if (i < copy.length) {
+                    row.push(copy[i]);
+               }
+          }
+          this.push(row);
+     }
+};
+
+
 
 const asyncForEach = async (array, callback) => {
      for (let index = 0; index < array.length; index++) {
